1739a2b0819500a6a5a908ce0bfe95bea03a0b66
ltr <apuroopgupca007@gmail.com>
2023-10-08 10:11:52 +0530

    Configured React router dom and added Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { Provider } from "react-redux";
 import "./App.css";
-import Body from "./components/Body";
-import Head from "./components/Head";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import AppLayout from "./components/AppLayout";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
       <div>
-        <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
---
+++ src/components/AppLayout.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import Head from "./Head";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+};
+
+export default AppLayout;
